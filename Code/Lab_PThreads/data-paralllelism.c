df7bc99ee4afa96fadb807d2b293734577af4469
Paolo Burgio <paolo.burgio@gmail.com>
2024-11-18 14:38:28 +0100

    Modified 07, added slides on data parallelism

--- Code/Lab_PThreads/data-paralllelism.c
+++ Code/Lab_PThreads/data-paralllelism.c
@@ -2,16 +2,28 @@
 #include <pthread.h>
 
 #define NUM_THREADS 16
+#define SIZE 16
+
+int arr[SIZE];
 
 /*
  * This function is the code of PThread
  */
 void * my_pthread_fn(void* args)
 {
-    // Unbox/unmarshall my (user defined) ID
+    // UNBOXING my (user defined) ID
     int my_id = (int) args; // This cast might generate a warning
     
     printf("\t\t\t\t\tHello World! I am a thread %d, and the OS assigned me the ID %lu\n", my_id, pthread_self());
+
+    // Do my work. I use my ID as array index. So,
+    //  Thread 0 -> arr[0]
+    //  Thread 1 -> arr[1]
+    //  Thread 2 -> arr[2]
+    //  ...and so on...
+    //
+    // This means, that the amount of work (my CHUNK) is 1
+    arr[my_id] = arr[my_id] * 2;
 
     return NULL;
 }
@@ -21,6 +33,16 @@ int main()
     pthread_t mythread[NUM_THREADS];
     pthread_attr_t attr[NUM_THREADS];
     int res;
+
+    // Init my array
+    for (unsigned int i = 0; i < SIZE; i++)
+        arr[i] = i;
+
+    // This is the work that I want to parallelize
+    // You do data-paralelism when you want to accelerate loops
+    //
+    // for(unsigned int i=0; i<SIZE; i++)
+    //     arr[i] = arr[i] * 2;
 
     printf("Hello World! I am the MASTER thread, before creating slaves\n");
     for (unsigned int i = 1; i < NUM_THREADS; i++)      // ==> FORK
@@ -32,7 +54,7 @@ int main()
                               my_pthread_fn,
                               // Define thread Id (user-defined), from 1 onwards,
                               // and pass it as args (boxing/marshalling)
-                              (void *) i // This cast might generate a warning
+                              (void *) i // BOXING. This cast might generate a warning
                              );
         
         if(res != 0)
@@ -53,6 +75,31 @@ int main()
         res = pthread_join (mythread[i], &returnvalue);
 
     printf("Hello World! I am the MASTER thread, after joining slaves\n");
-    
+
+    // Check results
+    for (unsigned int i = 0; i < SIZE; i++)
+        printf("arr[%u] is %d\n", i, arr[i]);
+
+    // Additional note. Checking the correctness of results by printing them on screen
+    // is not efficient, because you cannot automate this test. For this reason,
+    // most of engineers prefer to create a checksum and assess against it.
+
+    /*
+    int expected_checksum = 0, checksum = 0;
+    for (int i=0; i<SIZE; i++)
+        expected_checksum += (i*2);
+
+    for (int i=0; i<SIZE; i++)
+        checksum += arr[i];
+
+    if(checksum == expected_checksum)
+        printf("--- Checksum correct! Good job!\n");
+    else
+        printf("--- Checksum FAIL! Your program has something wrong.\n");
+
+    // ..or..
+    // assert(expected_checksum, checksum);
+    */
+
     return 0;
 }
